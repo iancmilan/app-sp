3b03e5f109b25479fb223ed1de2e910210422891
ian <iancmilan@unochapeco.edu.br>
2020-10-10 15:33:20 -0300

    Account page

--- src/pages/Account/index.tsx
+++ src/pages/Account/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import PageHeader from '../../components/PageHeader';
+
+import avatarImg from '../../assets/avatar.png';
 
 import styles from './styles';
 
@@ -9,6 +12,16 @@ function Account() {
     return (
         <View style={styles.container}>
             <PageHeader title="Minha conta" needGoBackButton={false}/>
+            <View style={styles.profile}>
+                <Image  style={styles.avatar} source={avatarImg} resizeMode="contain"></Image>
+                <Text style={styles.name}>Nome: Ian Carlos Milan</Text>
+                <Text style={styles.email}>E-mail: iancmilan@unochapeco.edu.br</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+                <RectButton style={styles.buttonLogOut}>
+                    <Text style={styles.buttonLogOutText}>Sair</Text>
+                </RectButton>
+            </View>
         </View>
     );
 }
--- src/pages/Account/styles.ts
+++ src/pages/Account/styles.ts
@@ -2,7 +2,42 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     container : {
-        
+        flex: 1,
+    },
+    profile: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 70,
+    },
+    avatar: {
+        width: 140,
+        height: 140,
+        borderRadius: 70,
+    },
+    name: {
+        marginTop: 20,
+        fontSize: 15,
+    },
+    email: {
+        marginTop: 15,
+        fontSize: 15,
+    },
+    buttonContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        marginBottom: 36,
+    },
+    buttonLogOut: {
+        backgroundColor: '#FF4D4F',
+        width: 300,
+        height: 40,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonLogOutText: {
+        color: '#FFF',
     },
 });
 
